adcd6dd870c14b9074d75c36fcf9019e77faadc0
Paulo-Tonelotti <paulohenrique63664@gmail.com>
2023-04-17 01:02:12 -0300

    criando acc

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from "uuid";
+
+
+class User {
+  id: string;
+
+  name: string;
+
+  username: string;
+
+  email: string;
+
+  password: string;
+
+  driver_license: string;
+
+  admin: boolean;
+
+  created_at: Date;
+
+  constructor() {
+    if(!this.id){
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { User };
