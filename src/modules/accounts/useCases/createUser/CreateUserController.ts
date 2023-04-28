67fc4b5b5bacac49d203becb86b9d4eb57a42c7c
Paulo-Tonelotti <paulohenrique63664@gmail.com>
2023-04-28 16:02:14 -0300

    alter create user

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -4,18 +4,17 @@ import { container } from "tsyringe";
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const  { name, email, driver_license, password} = request.body;
+    const { name, email, driver_license, password } = request.body;
     const createUserUseCase = container.resolve(CreateUserUseCase);
 
     await createUserUseCase.execute({
-      name,  
-      email, 
-      driver_license, 
-      password
-    })
+      name,
+      email,
+      driver_license,
+      password,
+    });
 
     return response.status(201).send();
-    
   }
 }
 
